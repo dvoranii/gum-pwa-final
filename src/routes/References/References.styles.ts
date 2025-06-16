1072b72d85b5da5035f465aed14a631415b6b78d
dvoranii <ildidvorani@gmail.com>
2025-06-16 13:24:16 -0400

    updated font size

--- src/routes/References/References.styles.ts
+++ src/routes/References/References.styles.ts
@@ -7,15 +7,20 @@ export const ReferencesContainer = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    // align-items: center;
     padding: 0 6.4rem;
     p {
         font-family: "Gotham", sans-serif;
-        font-size: clamp(18px, 1.8vw, 2.4rem);
+        font-size: clamp(18px, 1.4vw, 1.8rem);
         color: ${colors.black};
 
         sup {
             font-size: 24px;
         }
+    }
+
+    @media screen and (min-width: 2300px) {
+      p {
+        font-size: clamp(18px, 1.8vw, 2.4rem);
+      }  
     }
 `;

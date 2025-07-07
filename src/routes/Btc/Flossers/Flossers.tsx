0f4eed10e63b91eb94617313f81fe6f3431e333b
dvoranii <ildidvorani@gmail.com>
2025-07-07 15:21:37 -0400

    added gap to flosser slide 2

--- src/routes/Btc/Flossers/Flossers.tsx
+++ src/routes/Btc/Flossers/Flossers.tsx
@@ -107,7 +107,7 @@ export default function BtcFlossers(){
             {/* Slide 2 */}
 
              <BaseCarouselSlide>
-                <S.SlideWrapper $marginLeft="24px">
+                <S.SlideWrapper $marginLeft="24px" $gap="0.3rem">
                     <S.SlideColumn1>
                         <S.TitleListWrapper $paddingTop="0">
                             <h4><span>Professional</span> <span>Clean Plus</span><br/> <span>Flosser Picks</span> <span>Mint</span></h4>
